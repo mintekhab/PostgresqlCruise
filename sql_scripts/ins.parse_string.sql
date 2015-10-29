f423c823de5a46717a26eb095c35d817c200ce7d
mintekhab <mintekhab@expedia.com>
2015-10-29 15:55:11 +0530

    add check for null in parse function

--- sql_scripts/ins.parse_string.sql
+++ sql_scripts/ins.parse_string.sql
@@ -6,11 +6,25 @@ DECLARE
   sql        varchar;
   regex      varchar;
 BEGIN
-    regex := 'E''\\'||$2||'''';
-    sql := 'SELECT val FROM regexp_split_to_table( '|| quote_literal($1) || ',' ||  regex || ') as val' ;
-    raise notice 'sql query : %', sql;
-    return query execute sql;
+      IF  ($1 is not null )
+      AND ($2 is not null ) THEN 
+        regex := 'E''\\'||$2||'''';
+        sql := 'SELECT val FROM regexp_split_to_table( '|| quote_literal($1) || ',' ||  regex || ') as val' ;
+        raise notice 'sql query : %', sql;
+        return query execute sql;
+      ELSIF ($1 is null ) THEN
+        raise notice 'instring is null  : %', $1;
+        raise Exception 'Function aborted due to null input string' ;
+      ELSIF ($2 is null ) THEN
+        raise notice 'delimiter is null  : %', $2 ;
+        raise Exception 'Function aborted due null delimiter' ;
+      END IF ; 
+    
 END;
 $$
 language 'plpgsql'
 ;
+    
+--SELECT * FROM ins.parse_string('1,2,3'::varchar,NULL);
+--SELECT * FROM ins.parse_string(NULL,NULL);
+--SELECT * FROM ins.parse_string('A,B,C,D,E,1,2,3,'::varchar,','::char);
